106f0cd0e886e9615770b2103ad935a266a9dc6d
eszoke <evan.szoke@gmail.com>
2017-12-02 20:08:34 -0800

    install script update

--- arch-install.sh
+++ arch-install.sh
@@ -2,6 +2,7 @@
 
 # Global constants
 readonly ERR_MSG="Invalid selection. Please try again."
+readonly SUCCESS_MSG="done\n"
 readonly SUCCESS=0
 readonly FAILURE=1
 readonly PING_ONCE=1
@@ -70,7 +71,7 @@ check_boot () {
     # UEFI vars detected
     if [ "$(ls -A ${UEFI_DIR})" ]; then
         #User selected BIOS mode with UEFI vars, abort
-        if [ "$1" = "BIOS" ]; then
+        if [ "$1" = "msdos" ]; then
             printf "Invalid selection, UEFI boot detected.\n"
             printf "Please reboot ISO in legacy BIOS mode to continue.\n"
             abort force
@@ -79,7 +80,7 @@ check_boot () {
     # UEFI vars not detected
     else
         # User selected UEFI mode without UEFI vars, abort
-        if [ "$1" = "UEFI" ]; then
+        if [ "$1" = "gpt" ]; then
             printf "Invalid selection, BIOS boot detected.\n"
             printf "Please reboot ISO in UEFI mode to continue.\n"
             abort force
@@ -93,7 +94,7 @@ ping_test () {
     # Ping Google
 
     # If ping succeeds
-    if ! ping -c ${PING_ONCE} www.google.com &> /dev/null; then
+    if ping -c ${PING_ONCE} www.google.com &> /dev/null; then
         return ${SUCCESS}
 
     # If ping fails on second (last) attempt
@@ -153,7 +154,7 @@ check_network () {
                 wait
                 dhcpcd "${net_dev}"
                 error_check "Retrieved IP address via DHCP: Success\n"
-
+                error_check
                 break;;
 
             "Wireless (WLAN)")
@@ -163,7 +164,7 @@ check_network () {
                 printf "Getting wireless device......"
                 net_dev="$(find /sys/class/net | grep -i '/w')"
                 net_dev="${net_dev:15}" # Parse out directories from string
-                error_check "done\nCurrent network device is '${net_dev}'"
+                error_check "'${SUCCESS_MSG}'Current network device is '${net_dev}'"
 
                 # If no wireless device detected, go back to menu
                 if [ "${net_dev}" = "" ]; then
@@ -174,12 +175,12 @@ check_network () {
                 # Attempt to turn wireless device on
                 printf "Turning on wireless device '%s'......" "${net_dev}"
                 ip link set "${net_dev}" up
-                error_check "done\n"
+                error_check "$SUCCESS_MSG"
 
                 # Scan for wireless access points
                 printf "Scanning for wifi access points......"
                 iw dev "${net_dev}" scan | less
-                error_check "done\n"
+                error_check "$SUCCESS_MSG"
 
                 # Prompt user for wifi SSID and passphrase
                 read -r -p "Enter your wifi SSID: " wpa_ssid
@@ -188,7 +189,7 @@ check_network () {
                 # Attempt to connect to wireless access point
                 printf "Attempting to connect to '%s'......" "${wpa_ssid}"
                 wpa_supplicant -B -i "${net_dev}" -c <(wpa_passphrase "${wpa_ssid}" "${wpa_psk}")
-                error_check "done\n"
+                error_check "$SUCCESS_MSG"
 
                 # Attempt to get IP via DHCP
                 printf "Attempting to retrieve IP address......\n"
@@ -215,22 +216,100 @@ disk_partitioner () {
     local options opt boot="$1"
 
     # Disk prompt
-    PS3="Select an install type: "
-    options=("Linux boot only" "Dual-boot with Windows (same drive)" "Dual-boot with Windows (separate drive")
-    select opt in "{options[@]}"; do
-        case $opt in
-            "Linux boot only")
-                break;;
-            "Dual-boot with Windows (same drive)")
-                break;;
-            "Dual-boot with Windows (separate drive)")
-                break;;
-            *) # Invalid input
-                printf "\n%s\n\n" "${ERR_MSG}"
-                continue;;
-        esac
+#    PS3="Select an install type: "
+#    options=("Linux boot only" "Dual-boot with Windows (same drive)" "Dual-boot with Windows (separate drive")
+#    select opt in "{options[@]}"; do
+#        case $opt in
+ #           "Linux boot only")
+#                break;;
+#            "Dual-boot with Windows (same drive)")
+#                break;;
+#            "Dual-boot with Windows (separate drive)")
+#                break;;
+#            *) # Invalid input
+#                printf "\n%s\n\n" "${ERR_MSG}"
+#                continue;;
+#        esac
+ #   done
+
+    while true; do
+
+        # Print disks for user
+        parted -l
+
+        # Prompt the user to select a disk
+        disks="$(parted -l | grep '/sd')"
+        oldIFS=$IFS
+        IFS=$'\n'
+        options=( $disks )
+        IFS=$oldIFS
+        PS3="Please choose the drive where Arch will be installed: "
+        select opt in "${options[@]}"; do
+            for disk in "${options[@]}"; do
+                if [[ "$disk" = "$opt" ]]; then
+                    break 2
+                fi
+            done
+        done
+
+        install_disk="${opt:5:8}"
+    
+        printf "Arch Linux will be installed on '%s'.\n" "$opt"
+        read -r -p "WARNING: THIS WILL DELETE EVERYTHING ON THIS DRIVE. CONTINUE? (y/N):" input
+        if [ "$input" = "y" ] || [ "$input" = "Y" ] || [ "$input" = "yes" ]; then
+            break
+        fi
     done
 
+    # Get swap info from user
+    read -r -p "Enter swap size in GB: " swap_size
+    let swap_size*=1024
+    let swap_size+=513
+
+    printf "Creating new '%s' partition table......" "$boot"
+    parted -s "$install_disk" mkabel gpt
+    error_check "$SUCCESS_MSG"
+
+    printf "Creating EFI partition......"
+    parted -s -a optimal "$install_disk" mkpart ESP fat32 1MiB 513MiB
+    error_check "$SUCCESS_MSG"
+
+    printf "Setting boot/ESP flag......"
+    parted -s "$install_disk" set 1 esp on
+    error_check "$SUCCESS_MSG"
+
+    printf "Creating swap partition......"
+    parted -s -a optimal "$install_disk" mkpart primary linux-swap 513MiB ${swap_size}MiB
+    error_check "$SUCCESS_MSG"
+
+    printf "Creating root partition......"
+    parted -s -a optimal "$install_disk" mkpart primary ext4 ${swap_size}MiB 90%
+    error_check "$SUCCESS_MSG"
+
+    printf "Formatting EFI partition to FAT32......"
+    mkfs.fat -F32 "${install_disk}1"
+    error_check "$SUCCESS_MSG"
+
+    printf "Formatting swap partition......"
+    mkswap "${install_disk}2"
+    error_check "$SUCCESS_MSG"
+
+    printf "Formatting root partition to ext4......"
+    mkfs.ext4 "${install_disk}3"
+    error_check "$SUCCESS_MSG"
+
+    printf "Mounting swap partition......"
+    swapon "${install_disk}2"
+    error_check "$SUCCESS_MSG"
+
+    printf "Mounting root partition (/mnt)......"
+    mount "${install_disk}3" /mnt
+    error_check "$SUCCESS_MSG"
+
+    printf "Mounting EFI partition (/mnt/boot)......"
+    mkdir /mnt/boot
+    mount "${install_disk}1" /mnt/boot
+    error_check "$SUCCESS_MSG"
 }
 
 #===Main Program=============
@@ -264,7 +343,7 @@ main () {
         esac
     done
 
-    # Check for a valid boot given the user's selection
+    # Check for a valid boot given the users selection
     check_boot ${boot_mode}
 
     # Check for a working internet connection
@@ -278,6 +357,24 @@ main () {
     # Partition the disks
     disk_partitioner ${boot_mode}
 
+    # Sort the download mirrors by speed
+    printf "Sorting pacman mirrors by download speed......"
+    reflector --country "United States" --country "Canada" --age 12 -- protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+    error_check "done\n"
+
+    # Bootstrap base Arch packages plus essentials
+    printf "Installing base Arch Linux packages...\n"
+    pacstrap /mnt base base-devel grub git openssh vim
+    error_check "Bootstrap completed successfully\n"
+
+    # Generate fstab file
+    printf "Generating fstab file......"
+    genfstab -U /mnt >> /mnt/etc/fstab
+    error_check
+
+    # Change root into new install
+    arch-chroot /mnt
+    echo "u there?"
     # Temp ending
     printf "\nlol u made it\n"
 }
